104e45514f5f49183b30a2e323dfc345d9df0499
Mars Le <hp.mars.le@gmail.com>
2019-08-18 20:20:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,15 +19,15 @@ def third_challenge
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
-        :age = "50"
+        :age => "50"
         },
       :matriarch => {
         :name => "Lady Capulet",
-        :age = "51"
+        :age = >"51"
         },
       :heroine => {
         :name => "Juliet",
-        :age = "15",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
